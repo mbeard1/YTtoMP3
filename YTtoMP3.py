4c3be2079c9d10c4381de4d16fa18be9d4eeda2f
mbeard1 <mbeard875@gmail.com>
2022-03-16 20:33:12 -0400

    Added support for video downloads

--- YTtoMP3.py
+++ YTtoMP3.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser as ap
 import os
 import subprocess as sp
-from pytube import YouTube
+import pytube
 from pytube.cli import on_progress
 
 
@@ -30,17 +30,18 @@ def rip_audio(destination: str):
 
 def yt_dl(url: str, destination: str):
 
-    yt = YouTube(url)
+    yt = pytube.YouTube(url)
     yt.register_on_progress_callback(on_progress)
-    yt.streams.get_highest_resolution().download(destination, filename='tempvideo')
+    yt.streams.get_highest_resolution().download(destination, filename='tempvideo.mp4')
 
     return
 
 
-def yt_to_mp3(url: str, destination: str):
+def yt_to_mp3(url: str, destination: str, video: bool):
 
     yt_dl(url, destination)
-    rip_audio(destination)
+    if not video:
+        rip_audio(destination)
 
     return
 
@@ -50,8 +51,10 @@ if __name__ == '__main__':
     parser = ap(description='Rip Audio From Given YouTube Video')
     parser.add_argument('-u', '--url', type=str, help='URL for the youtube video', required=True)
     parser.add_argument('-d', '--destination', type=str, help='Destination for the output file', required=True)
+    parser.add_argument('-v', '--video', action='store_true')
 
     args = parser.parse_args()
 
-    yt_to_mp3(args.url, args.destination)
-    cleanup(args.destination)
+    yt_to_mp3(args.url, args.destination, args.video)
+    if not args.video:
+        cleanup(args.destination)
